ce35cf2dd33a86d211e3972d40be4e76499a834a
dali <dali@ufofactory.org>
2015-11-20 14:43:33 +0900

    bugfix 이전 대자보 question 기본 값 처리

--- db/migrate/20151120043158_init_sources.rb
+++ db/migrate/20151120043158_init_sources.rb
@@ -1,5 +1,9 @@
 class InitSources < ActiveRecord::Migration
   def up
+    Poster.where(question: nil).each do |p|
+      p.question = 'What do you think about it?'
+      p.save!
+    end
     Poster.where(source: nil).each do |p|
       p.source = Source.find_or_create_by(url: p.url) do |s|
         s.assign_attributes(title: p.read_attribute(:title), description: p.read_attribute(:description), image: p.read_attribute(:image))
